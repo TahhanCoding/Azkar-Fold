2483ee2620a7e5bee4cb3a51e26ec6e1c79de11d
Ahmed Sha'ban <a.sh.tahan@gmail.com>
2025-05-19 04:11:18 +0300

    Added id to zekr model

--- Azkar Fold/Azkar Fold/Models/SunnahZekrItem.swift
+++ Azkar Fold/Azkar Fold/Models/SunnahZekrItem.swift
@@ -5,7 +5,7 @@ struct SunnahAzkar: Decodable {
     let content: [SunnahZekrItem]
 }
 struct SunnahZekrItem: Identifiable, Decodable {
-    var id = UUID() 
+    let id: Int
     let zekr: String
     let transliteration: String
     let en_tr: String
@@ -14,7 +14,7 @@ struct SunnahZekrItem: Identifiable, Decodable {
     let bless_en: String?
 
     enum CodingKeys: String, CodingKey {
-        case zekr, `repeat`, bless
+        case id, zekr, `repeat`, bless
         case transliteration = "zekr_transliteration"
         case bless_en = "bless_english_translation"
         case en_tr = "english_translation"
